8b740b8941aa688505294fd0ebf4407dc7e2f511
Minin <70429604+MininxD@users.noreply.github.com>
2022-06-21 20:52:38 -0700

    Update this.js

--- utils/language/this.js
+++ utils/language/this.js
@@ -14,22 +14,18 @@ return`*Ada Kesalahan!\n*Report To Admin:*\n*wa.me/+6289501303263*`
 exports.menu = (prefix,pushname,sender,bbv,premi,nodes,own,times) => {
 return`*Whatsapp Bot*
 
-*</info>*
-*Halo Kak ${pushname}👋*
-*Uptime*: ${bbv(nodes)}
+*_info_*
+*Halo Kak 👋*
+*Server Uptime*: ${bbv(nodes)}
 *Sender*: @${sender.split("@")[0]}
 *Time*: ${times}
-*Prefix*: No Prefix
-
-*</owner>*
 *Owner*: @${own.split("@")[0]}
 
-*</menu>*
+*_menu_*
 *${prefix}sticker* _send image/video_
 *${prefix}eval* _javascript_
 *${prefix}takestick* _pack|author_
 *${prefix}toimg* _tag sticker_
-*${prefix}listpremium*
 *${prefix}play* _text_
 *${prefix}joox* _lagu_
 *${prefix}wiki* _[object]_
